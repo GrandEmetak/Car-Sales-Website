093a9035f0b9f54ac7012dbbf55ea97c43ad2549
SlartiBartFast-art <JekHamond@tutanota.com>
2022-02-14 15:25:56 +0500

    Wrapper class + innerHolder #01

--- src/main/java/ru/job4j/cars/store/PostRepository.java
+++ src/main/java/ru/job4j/cars/store/PostRepository.java
@@ -1,28 +1,26 @@
 package ru.job4j.cars.store;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
-import ru.job4j.cars.connect.DBSession;
+
 import ru.job4j.cars.model.Photo;
 import ru.job4j.cars.model.Post;
 
 import java.util.*;
-import java.util.function.Function;
 
 /**
  * Основной класс по работе с БД через hibernate
- * Реализовать пользовательские фильтры для площадок машин [#4745]
- * Уровень : 3. МидлКатегория : 3.3. HibernateТопик : 3.3.3. HQL, Criteria
+ * +
+ * Wrapper Класс предоставляет SessionFactory через class DBSession
+ * Реализованы пользовательские фильтры для площадок машин
  * - показать объявления за последний день
  * - показать объявления с фото
  * - показать объявления определенной марки.
- * Реализовать площадку продаж машин. [#4747]
- * Уровень : 3. МидлКатегория : 3.3. HibernateТопик : 3.3.2. Mapping
+ * и др.
  * Logger LOGGER = Logger.getLogger(имя класса для которго мы получаем Логгер);
  */
 public class PostRepository implements Store {
@@ -35,41 +33,12 @@ public class PostRepository implements Store {
 
     private static final Marker DEBUG = MarkerFactory.getMarker("DEBUG");
 
-    private static SessionFactory sf = DBSession.getInstance();
+    private final Wrapper wrapper = new Wrapper();
 
     public static PostRepository instOf() {
         return INST;
     }
 
-    /**
-     * Лямбды и шаблон wrapper.
-     * Здесь мы можем применить шаблон проектирования wrapper.
-     * Function<T,R>
-     * Функциональный интерфейс Function<T,R> представляет функцию перехода
-     * от объекта типа T к объекту типа R
-     * Метод apply()- это основной абстрактный функциональный метод Function интерфейса.
-     * Он принимает в качестве входных данных параметр T типа  и выдает выходной объект типа R.
-     * Interface EntityTransaction.commit()
-     * - Зафиксируйте текущую транзакцию ресурса, записав все незатронутые изменения в базу данных.
-     *
-     * @param command
-     * @param <T>
-     * @return
-     */
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sf.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-    }
 
     /**
      * save Post Object in to DB Post
@@ -90,7 +59,7 @@ public class PostRepository implements Store {
     private Post saveByPost(Post post) {
         Post rsl = null;
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             session.beginTransaction();
             var result = session.save(post);
             int index = (int) result;
@@ -108,7 +77,7 @@ public class PostRepository implements Store {
     private Post updateByPost(Post post) {
         Post rsl = null;
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             session.beginTransaction();
             session.saveOrUpdate(post);
 
@@ -127,7 +96,7 @@ public class PostRepository implements Store {
     public Photo savePhoto(Photo photo) {
         Photo rsl = null;
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             session.beginTransaction();
             var result = session.save(photo);
             int index = (int) result;
@@ -149,7 +118,7 @@ public class PostRepository implements Store {
      */
     @Override
     public List<Post> findAllPost() {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery("from ru.job4j.cars.model.Post");
                     LOGGER.debug(DEBUG, "info -> all Post method PostRepository {}");
@@ -165,7 +134,7 @@ public class PostRepository implements Store {
      */
     @Override
     public List<Post> lastDay() {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery("from ru.job4j.cars.model.Post s"
                             + " where s.created between :start and :finish"
@@ -199,7 +168,7 @@ public class PostRepository implements Store {
     @Override
     public List<Post> whenPhotoTrue() {
         List<Post> postList = new ArrayList<>();
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery(
                             "select distinct st from Post st join fetch st.photo a "
@@ -218,7 +187,7 @@ public class PostRepository implements Store {
      */
     @Override
     public List<Post> whenMarkAuto(String name) {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery(
                             "select distinct st from Post st "
@@ -243,7 +212,7 @@ public class PostRepository implements Store {
     public List<Post> findPostBiId(int id) {
         List<Post> postList = new ArrayList<>();
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             session.beginTransaction();
             var query = session.createQuery(
                     "select distinct st from Post st "
@@ -269,7 +238,7 @@ public class PostRepository implements Store {
     public List<Post> findPostByUserId(int id) {
         List<Post> postList = new ArrayList<>();
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             session.beginTransaction();
             var query = session.createQuery(
                     "select distinct st from Post st "
@@ -297,7 +266,7 @@ public class PostRepository implements Store {
      */
     @Override
     public List<Post> findPostByUserIdAndHeader(int id, String header) {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery(
                             "select distinct st from Post st "
@@ -324,7 +293,7 @@ public class PostRepository implements Store {
         boolean rsl = false;
         if (id != 0) {
             try {
-                Session session = sf.openSession();
+                Session session = wrapper.getSf().openSession();
                 Transaction tx = session.beginTransaction();
                 Post post = (Post) session.load(Post.class, id);
                 session.delete(post);
--- src/main/java/ru/job4j/cars/store/UserRepository.java
+++ src/main/java/ru/job4j/cars/store/UserRepository.java
@@ -1,59 +1,28 @@
 package ru.job4j.cars.store;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
-import ru.job4j.cars.connect.DBSession;
+
 import ru.job4j.cars.model.User;
 
 import java.util.List;
-import java.util.function.Function;
 
 /**
  * Слой репозиторий по работе с БД and table users
+ * +
+ * Wrapper Класс предоставляет SessionFactory через class DBSession
  */
 public class UserRepository implements UserRepoStore {
+
     private static final UserRepository INST = new UserRepository();
 
-    private final SessionFactory sf = DBSession.getInstance();
+    private final Wrapper wrapper = new Wrapper();
 
     public static UserRepository instOf() {
         return INST;
     }
 
-    /**
-     * Лямбды и шаблон wrapper.
-     * Здесь мы можем применить шаблон проектирования wrapper.
-     * Function<T,R>
-     * Функциональный интерфейс Function<T,R> представляет функцию перехода
-     * от объекта типа T к объекту типа R
-     * Метод apply()- это основной абстрактный функциональный метод Function интерфейса.
-     * Он принимает в качестве входных данных параметр T типа  и выдает выходной объект типа R.
-     * Interface EntityTransaction.commit()
-     * - Зафиксируйте текущую транзакцию ресурса, записав все незатронутые изменения в базу данных.
-     *
-     * @param command
-     * @param <T>
-     * @return
-     */
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sf.openSession();
-        final Transaction tx = session.beginTransaction();
-        try {
-            T rsl = command.apply(session);
-            tx.commit();
-            return rsl;
-        } catch (final Exception e) {
-            session.getTransaction().rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
-    }
 
     /**
      * RegServlet doPost usage
@@ -66,7 +35,7 @@ public class UserRepository implements UserRepoStore {
     public User saveUser(User user) {
         User rsl = null;
         try {
-            Session session = sf.openSession();
+            Session session = wrapper.getSf().openSession();
             Transaction tx = session.beginTransaction();
             var result = session.save(user);
             int index = (int) result;
@@ -89,7 +58,7 @@ public class UserRepository implements UserRepoStore {
      */
     @Override
     public List<User> findUserById(int id) {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     var query = session.createQuery("from ru.job4j.cars.model.User s "
                             + "where s.id = :sId")
@@ -110,7 +79,7 @@ public class UserRepository implements UserRepoStore {
      */
     @Override
     public List<User> findByEmail(String email) {
-        return this.tx(
+        return this.wrapper.tx(
                 session -> {
                     String sql = "from ru.job4j.cars.model.User where email = :email";
                     Query query = session.createQuery(sql);
